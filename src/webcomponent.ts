94e98fb812bf786c79b4f3974855c180fea31d7c
enzoaicardi <enzo.aicardi@gmail.com>
2024-08-21 21:29:37 +0200

    [doc] update jsdoc

--- src/webcomponent.ts
+++ src/webcomponent.ts
@@ -18,6 +18,10 @@ export class WebComponent extends WebComponentConnector {
     /** @internal */
     static [Symbols.defined] = new Set<WebComponent>();
 
+    /**
+     * <Client> defines a custom element in window's CustomElementRegistry
+     * @param {WebComponent} definition WebComponent subclass definition
+     */
     static define(definition: any) {
         // csr only
         if (definition && !WebComponent[Symbols.defined].has(definition)) {
@@ -29,35 +33,60 @@ export class WebComponent extends WebComponentConnector {
         }
     }
 
+    /**
+     * <Client|Server> method used to obtain a WebComponent instance
+     * @param args arguments to pass to the WebComponent constructor
+     * @returns {HTMLElement}
+     */
+    static createElement(...args: any[]): HTMLElement {
+        if (!isServer) {
+            // define the WebComponent subclass to prevent "Illegal constructor" errors
+            WebComponent.define(this);
+        }
+        // return the current WebComponent subclass as HTMLElement
+        return new this(...args) as unknown as HTMLElement;
+    }
+
+    /**
+     * create a WebComponent
+     */
     constructor(...args: any[]) {
         super();
         this[Symbols.args] = args;
     }
 
+    /**
+     * <Client> native custom element connectedCallback method
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements|MDN}
+     * @returns {HTMLElement}
+     */
     connectedCallback(): HTMLElement {
         // csr only
+        // get the current WebComponent subclass as HTMLElement
         const self = this as unknown as HTMLElement;
 
+        // if the WebComponent is empty
         if (!self.childNodes.length) {
+            // get the childNodes fragment
             const childNodesFragment = this[Symbols.childNodes]();
+            // if there is a fragment append the fragment as WebComponent child
             if (childNodesFragment) {
                 self.append(childNodesFragment);
             }
         }
 
+        // TypeScript concerns
+        // return the current WebComponent subclass
         return self;
     }
 
+    /**
+     * <Client|Server> method used to represent the child content of a WebComponent
+     * @returns {string}
+     */
     render(): string {
         // csr & ssr
         return "";
-    }
-
-    static createElement(...args: any[]): HTMLElement {
-        if (!isServer) {
-            WebComponent.define(this);
-        }
-        return new this(...args) as unknown as HTMLElement;
     }
 
     /** @internal */
@@ -87,10 +116,16 @@ export class WebComponent extends WebComponentConnector {
         }
     }
 
-    /** @internal */
+    /**
+     * <Client|Server> method used to return the string representation of the WebComponent
+     * @internal
+     * @returns {string}
+     */
     [Symbols.stringPrimitive](): string {
         // csr & ssr
+        // get the WebComponent tagName
         const tagName = this[Symbols.definition]["tagName"];
+        // build the string representation
         return tagName && `<${tagName}>${this.render()}</${tagName}>`;
     }
 
