31136572ac389d12e7e288d047699b8d30f3a176
enzoaicardi <enzo.aicardi@gmail.com>
2025-01-03 18:55:38 +0100

    [dev] fix toString return type

--- src/@enzoaicardi/webcomponent/shared.ts
+++ src/@enzoaicardi/webcomponent/shared.ts
@@ -32,22 +32,42 @@ export function renderRawComponent<T extends WebComponent>(
 
 export function createRawComponent<T extends WebComponent>(
     this: T
-): string | Promise<string> {
+): T extends Required<Pick<T, "render">>
+    ? string
+    : T extends Required<Pick<T, "renderAsync">>
+    ? Promise<string>
+    : never {
     // sync -> string
     if (this.render) {
-        return this[Symbols.raw](this.render());
+        return this[Symbols.raw](this.render()) as T extends Required<
+            Pick<T, "render">
+        >
+            ? string
+            : never;
     }
     // async -> Promise<string>
     else if (this.renderAsync) {
-        return this.renderAsync().then((raw) => this[Symbols.raw](raw));
+        return this.renderAsync().then((raw) =>
+            this[Symbols.raw](raw)
+        ) as T extends Required<Pick<T, "render">>
+            ? string
+            : T extends Required<Pick<T, "renderAsync">>
+            ? Promise<string>
+            : never;
     } else {
         throw new MissingRenderingMethod(this[Symbols.definition]);
     }
 }
 
-export function toPrimitiveComponent<T extends WebComponent>(this: T): string {
+export function toPrimitiveComponent<T extends WebComponent>(
+    this: T
+): T extends Required<Pick<T, "render">> ? string : never {
     if (this.render) {
-        return this[Symbols.raw](this.render());
+        return this[Symbols.raw](this.render()) as T extends Required<
+            Pick<T, "render">
+        >
+            ? string
+            : never;
     } else {
         throw new UnauthorizedCoercion(this[Symbols.definition]);
     }
