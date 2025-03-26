eb8170d0ff460171353f38d47040fa1317391a20
enzoaicardi <enzo.aicardi@gmail.com>
2025-03-26 21:23:36 +0100

    fix attrs

--- src/@enzoaicardi/webcomponent/shared.ts
+++ src/@enzoaicardi/webcomponent/shared.ts
@@ -9,12 +9,28 @@ export class WebComponentCore {
     /** @type {string} The element <tag-name> */
     public static tagName: string;
 
-    /** Element attributes */
-    attrs = new Map<string, any>();
-
     /** @internal */
     [Symbols.definition]: typeof WebComponentCore =
         Object.getPrototypeOf(this).constructor;
+
+    /** @internal */
+    [Symbols.raw](raw: string): string {
+        const tagName = this[Symbols.definition]["tagName"];
+
+        if (tagName) {
+            const attributes = Array.from(this.attrs).reduce(
+                (acc, [key, value]) =>
+                    acc + (/[^:.\w_-]/.test(key) ? "" : ` ${key}="${value}"`),
+                ""
+            );
+            return `<${tagName}${attributes}>${raw}</${tagName}>`;
+        } else {
+            throw new MissingTagName(this[Symbols.definition]);
+        }
+    }
+
+    /** Element attributes */
+    attrs = new Map<string, any>();
 
     /**
      * Method used to define the content of a WebComponent
@@ -27,26 +43,6 @@ export class WebComponentCore {
      * @returns {Promise<string>}
      */
     renderAsync?(): Promise<string>;
-
-    /** @internal */
-    [Symbols.raw](raw: string): string {
-        const tagName = this[Symbols.definition]["tagName"];
-
-        if (tagName) {
-            const attributes = this.attrs
-                ? Object.entries(this.attrs).reduce(
-                      (acc, [key, value]) =>
-                          acc +
-                          (/[^:.\w_-]/.test(key) ? "" : ` ${key}="${value}"`),
-                      ""
-                  )
-                : "";
-
-            return `<${tagName}${attributes}>${raw}</${tagName}>`;
-        } else {
-            throw new MissingTagName(this[Symbols.definition]);
-        }
-    }
 
     /**
      * Method used to return the string representation of the WebComponent
